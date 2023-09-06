76d819b15a592a505622f71d29e2dfdc1284fec2
HappyCandyZ <Zhang_Mujie666@163.com>
2023-09-06 11:31:51 -0400

    new update

--- typescript/src/renderer.ts
+++ typescript/src/renderer.ts
@@ -3,7 +3,7 @@ import { Shape } from "./shapes/shape.js";
 function newRenderer(rectangle: Shape) {
     return {
         draw() {
-            const area: number = rectangle.computeArea()
+            const area: number = rectangle.computeArea() //draw方法内定义area常量，值通过调用rectangle的computeArea方法来得到
             console.log("Shape drawn\n" + "Its area is " + area)
         }
     }
--- typescript/src/shapes/square.ts
+++ typescript/src/shapes/square.ts
@@ -1,8 +1,5 @@
 import { Shape } from "./shape";
 
-// Since a Square is a special type of Rectangle where width = height, 
-// we don't need to define a new interface. We can use the Rectangle interface for Square as well.
-
 function newSquare(sideLen: number): Shape {
     return {
         computeArea: function(): number {
